2a82a45f89d30e849937f3c7d9a845f01f8b2360
Francisco Santos <francisco.santos.lx81@gmail.com>
2021-04-10 15:28:08 +0100

    implemented CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # read/list all the tasks
+  get '/tasks', to: 'tasks#index'
+  # create one task
+  get '/tasks/new', to: 'tasks#new' # display the form
+  post '/tasks', to: 'tasks#create'
+  # edit one task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # read one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # remove a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
